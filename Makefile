dcd966ed3edd1e334482c65eec4017f8fc6b393c
Evgeniy Orekhov <super-nutman@yandex.ru>
2026-04-30 15:44:51 +0300

    make migrations

--- Makefile
+++ Makefile
@@ -2,3 +2,8 @@
 
 run:
 	go run cmd/main.go
+
+.PHONY: migrate
+
+migrate:
+	go run cmd/migrator/migrator.go
---
+++ cmd/migrator/migrator.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"time"
+	"weakRaider/internal/config"
+	"weakRaider/internal/database"
+
+	"github.com/pressly/goose/v3"
+	"github.com/rs/zerolog/log"
+
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	if err := config.ReadConfigYML("config.yml"); err != nil {
+		log.Fatal().Err(err).Msg("Failed init configuration")
+	}
+
+	cfg := config.GetConfigInstance()
+	migration := flag.Bool("migration", true, "Defines the migration start option")
+
+	dsn := fmt.Sprintf("host=%v port=%v user=%v password=%v dbname=%v sslmode=%v",
+		cfg.Database.Host,
+		cfg.Database.Port,
+		cfg.Database.User,
+		cfg.Database.Password,
+		cfg.Database.Name,
+		cfg.Database.SslMode,
+	)
+
+	initCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	db, err := database.NewPostgres(initCtx, dsn, cfg.Database.Driver, &cfg.Database.Connections)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed init postgres")
+	}
+	defer db.Close()
+
+	if *migration {
+		if err = goose.Up(db.DB, cfg.Database.Migrations); err != nil {
+			log.Error().Err(err).Msg("Migration failed")
+			return
+		}
+	}
+}
