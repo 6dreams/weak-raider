7786486fc92d078dc57e96d81ade17ab0e1bc25c
Evgeniy Orekhov <super-nutman@yandex.ru>
2026-05-01 14:53:11 +0300

    remove Repo intarface from repo

--- internal/repo/repo.go
+++ internal/repo/repo.go
@@ -1,21 +1,13 @@
 package repo
 
 import (
-	"context"
-	"weakRaider/internal/model"
-
 	"gorm.io/gorm"
 )
-
-type Repo interface {
-	Add(ctx context.Context, char model.Character) error
-	Upsert(ctx context.Context, char model.Character) error
-}
 
 type repo struct {
 	db *gorm.DB
 }
 
-func NewRepo(db *gorm.DB) Repo {
+func NewRepo(db *gorm.DB) *repo {
 	return &repo{db: db}
 }
