8ef69b86e32c048e159db481ec18398618dde6b5
Evgeniy Orekhov <super-nutman@yandex.ru>
2026-05-01 14:41:03 +0300

    #1 make same fields as in table

--- internal/model/model.go
+++ internal/model/model.go
@@ -3,19 +3,21 @@ package model
 import "time"
 
 type Guild struct {
-	Id           int       `db:"guild_id"`
-	Name         string    `db:"name"`
-	Wowaudit_key string    `db:"wowaudit_key"`
-	Updated      time.Time `db:"updated"`
+	Id          int       `db:"guild_id"`
+	Name        string    `db:"name"`
+	WowauditKey string    `db:"wowaudit_key"`
+	UpdatedAt   time.Time `db:"updated_at"`
 }
 
-type Champion struct {
-	Id          int       `db:"champion_id"`
-	Name        string    `db:"name"`
-	Class       string    `db:"class"`
-	Realm       string    `db:"realm"`
-	Note        string    `db:"note"`
-	Guild_id    int       `db:"guild_id"`
-	Blizzard_id int       `db:"blizzard_id"`
-	Updated     time.Time `db:"updated"`
+type Character struct {
+	Id         int       `db:"character_id"`
+	Name       string    `db:"name"`
+	Class      string    `db:"class"`
+	Rank       string    `db:"rank"`
+	Role       string    `db:"role"`
+	Realm      string    `db:"realm"`
+	Note       string    `db:"note"`
+	GuildId    int       `db:"guild_id"`
+	BlizzardId int       `db:"blizzard_id"`
+	UpdatedAt  time.Time `db:"updated_at"`
 }
