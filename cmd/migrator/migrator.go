f7f692c3414be010b0cd21900ee70ed7d7b0cdff
Evgeniy Orekhov <super-nutman@yandex.ru>
2026-05-21 19:06:06 +0300

    add migrate-down flag option

--- cmd/migrator/migrator.go
+++ cmd/migrator/migrator.go
@@ -51,5 +51,10 @@ func main() {
 			log.Error().Err(err).Msg("Migration-down failed")
 			return
 		}
+	} else {
+		if err = goose.Down(sqlDB, cfg.Database.Migrations); err != nil {
+			log.Error().Err(err).Msg("Migration failed")
+			return
+		}
 	}
 }
